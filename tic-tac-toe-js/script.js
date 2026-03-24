dc2c90b5a208b6509cd8bd6bb1776d17332d5eba
Arsh Sharma <arshsharma461@gmail.com>
2026-03-24 11:38:22 +0530

    game gets finished
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- tic-tac-toe-js/script.js
+++ tic-tac-toe-js/script.js
@@ -1,12 +1,41 @@
 let currentPlayer = "X";
+let gameRunning = true;
 let cells = document.querySelectorAll(".cell");
+let infoText = document.querySelector(".turn-info");
 cells.forEach((cell) => {
-    // console.log(cell);
     cell.addEventListener("click", () => onCellClick(cell));
 });
 
 function onCellClick(cell){
-    console.log(cell);
+    if (cell.innerText != "" || !gameRunning) {
+        return
+    }
     cell.innerText = currentPlayer;
-    currentPlayer = currentPlayer == "X" ? "O" : "X";
+    checkWinner()
+    if (gameRunning) {
+        currentPlayer = currentPlayer == "X" ? "O" : "X";
+        infoText.innerText = `${currentPlayer}'s Turn`
+    } else {
+        return;
+    }
+}
+
+function checkWinner(){
+    let winConditions = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+    winConditions.forEach((winCondition) => {
+        let [a,b,c] = [...winCondition];
+        console.log(cells[a].innerText);
+        console.log(cells[b].innerText);
+        console.log(cells[c].innerText);
+        if(cells[a].innerText != "" && cells[a].innerText == cells[b].innerText && cells[b].innerText == cells[c].innerText) {
+            console.log("CAME HERE")
+            infoText.innerText = `${currentPlayer} Wins`
+            gameRunning = false;
+            return;
+        }
+    })
 }
